2cee11b3c1cfe0f0b0c3660091b8913e69969b2f
Vyom-vartak <86948055+Vyom-V@users.noreply.github.com>
2023-06-28 10:59:43 +0530

    deploy bug fix

--- public/index.js
+++ public/index.js
@@ -2,7 +2,7 @@ const scoreBoard = document.getElementById("scoreBoard");
 const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
 
-const socket = io(); // initialize a new socket.io instance by passing the server object
+const socket = io("127.0.0.1:3000"); // initialize a new socket.io instance by passing the server object
 
 const devicePixelRatio = window.devicePixelRatio || 1;
 canvas.width = window.innerWidth * devicePixelRatio;
